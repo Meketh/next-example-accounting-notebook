aefc9d9199e1cd9d73dd2fda78f41060013f0c79
Nahuel Santoalla <meketh@pop-os.localdomain>
2020-03-17 18:28:44 +0100

    remove sample transactions

--- db/transactions.js
+++ db/transactions.js
@@ -1,9 +1,9 @@
 export const account = {balance: 0}
 export const transactions = [
-  {id: '111', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
-  {id: '222', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
-  {id: '333', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
-  {id: '444', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
-  {id: '555', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
-  {id: '666', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '111', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '222', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '333', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '444', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '555', type: 'credit', amount: 100, effectiveDate: '2020-03-17T14:33:24.517Z'},
+  // {id: '666', type: 'debit', amount: 200, effectiveDate: '2020-03-17T14:33:24.517Z'},
 ]
